14c52464ce9f94a60e44d4ad9d40975200084c0f
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2016-01-21 14:55:11 +0300

    Added --count and --trans options.
    Now the homework (well, at least the "blocks" part) is done like this:
    ./english_blocks.py --trans=off --count=10

--- english_blocks.py
+++ english_blocks.py
@@ -2,11 +2,25 @@
 
 __author__ = 'alexander'
 
+import argparse
 import json
 import codecs
 from random import shuffle
 
+option_set = "on"
+option_not_set = "off"
+
+
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description = "Coming soon")
+    arg_parser.add_argument('--trans',
+                            default = option_set,
+                            help="Turn this option on if you don't want the Russian translations to show up.")
+    arg_parser.add_argument('--count',
+                            default = "10",
+                            help="Number of entries to print.")
+    args = arg_parser.parse_args()
+
     f = codecs.open("english_blocks.json", "r", "utf-8")
     dict_tmp = json.loads(f.read())
     f.close()
@@ -15,6 +29,10 @@ if __name__ == '__main__':
         results.append((k, v))
     shuffle(results)
     f = codecs.open("output.txt", "w", "utf-8")
-    for i in results:
-        f.write(u"{0} : {1}\n".format(i[0], i[1]))
+    format_string = u"{0}"
+    if args.trans and args.trans == option_set:
+        format_string += u" : {1}"
+    format_string += u"\n"
+    for i in results[:int(args.count)]:
+        f.write(format_string.format(i[0], i[1]))
     f.close()
