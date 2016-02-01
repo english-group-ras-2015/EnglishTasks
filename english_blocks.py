b9bc407fcdc9d0840c4516deef5de2b9ea8e9d1d
Alexander <aleksiyantsa@gmail.com>
2016-02-01 23:17:56 +0300

    Start and end parameters added. Json format was changed. Refactoring

--- english_blocks.py
+++ english_blocks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 __author__ = 'alexander'
 
@@ -17,8 +18,17 @@ if __name__ == '__main__':
                             default = option_set,
                             help="Turn this option on if you don't want the Russian translations to show up.")
     arg_parser.add_argument('--count',
-                            default = "10",
+                            type = int,
+                            default = 10,
                             help="Number of entries to print.")
+    arg_parser.add_argument('--start',
+                            default = 0,
+                            type = int,
+                            help="The first block to include.")
+    arg_parser.add_argument('--end',
+                            default = 0,
+                            type = int,
+                            help="The last block to include.")
     args = arg_parser.parse_args()
 
     f = codecs.open("english_blocks.json", "r", "utf-8")
@@ -26,13 +36,15 @@ if __name__ == '__main__':
     f.close()
     results = list()
     for k, v in dict_tmp.iteritems():
-        results.append((k, v))
+        if (int(k) >= args.start and int(k) <= args.end) or (int(k) >= args.start and args.end == 0):
+            results.extend(v.items())
     shuffle(results)
     f = codecs.open("output.txt", "w", "utf-8")
     format_string = u"{0}"
     if args.trans and args.trans == option_set:
         format_string += u" : {1}"
     format_string += u"\n"
-    for i in results[:int(args.count)]:
+    length = args.count if args.count != 0 else len(results)
+    for i in results[:length]:
         f.write(format_string.format(i[0], i[1]))
     f.close()
